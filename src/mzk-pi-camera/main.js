c2eefc1573226b78ebc4ef26fa24d63801a01bae
muzkat <erik@muzkat.com>
2018-07-14 20:28:20 +0200

    Improvements, JSON API, refactoring - seperating api and service functions

--- src/mzk-pi-camera/main.js
+++ src/mzk-pi-camera/main.js
@@ -9,17 +9,43 @@ Ext.define('muzkat.pi.camera.Main', {
 
     layout: 'fit',
 
-    items: [{
-
-    }],
+    items: [{}],
 
     bbar: [{
         text: 'Take Picture',
-        iconCls: 'x-fa fa-photo'
+        iconCls: 'x-fa fa-photo',
+        handler: function (btn) {
+            var mainView = btn.up('mzkPiCameraMain');
+            if (Ext.isDefined(mainView) && mainView.isComponent) {
+                mainView.takePhoto().then(function (success) {
+                    Ext.toast(JSON.stringify(success));
+                }, function (error) {
+                    Ext.toast(error);
+                });
+            }
+        }
     }, {
         text: 'Gallery',
         iconCls: 'x-fa fa-file-image-o'
-    },{
+    }, {
         xtype: 'tbfill'
-    }]
+    }],
+
+    getAjax: function (url) {
+        return new Ext.Promise(function (resolve, reject) {
+            Ext.Ajax.request({
+                url: '/photos/take',
+
+                success: function (response) {
+                    resolve(Ext.decode(response.responseText, true));
+                },
+
+                failure: function (response) {
+                    // Use the provided "reject" method to deliver error message.
+                    //
+                    reject(response.status);
+                }
+            });
+        });
+    }
 });
