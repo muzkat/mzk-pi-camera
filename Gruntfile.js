7f916c5e6950cde90f68cabb4364bbc5dbb8e2ad
muzkat <erik@muzkat.com>
2018-07-17 20:08:38 +0200

    Improvements, JSON API, refactoring - seperating api and service functions

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,4 +41,5 @@ module.exports = function (grunt) {
 
     grunt.registerTask("dev", ["browserify:dev"]);
     grunt.registerTask('default', ['dev','watch']);
+    grunt.registerTask('build', ['dev']);
 };
--- src/mzk-pi-camera/main.js
+++ src/mzk-pi-camera/main.js
@@ -2,50 +2,84 @@
 Created by Erik Woitschig @devbnz
 */
 Ext.define('muzkat.pi.camera.Main', {
-    extend: 'Ext.panel.Panel',
+    extend: 'Ext.container.Container',
     alias: 'widget.mzkPiCameraMain',
 
     title: 'Muzkat Pi Camera',
 
-    layout: 'fit',
+    layout: 'center',
 
-    items: [{}],
-
-    bbar: [{
-        text: 'Take Picture',
-        iconCls: 'x-fa fa-photo',
-        handler: function (btn) {
-            var mainView = btn.up('mzkPiCameraMain');
-            if (Ext.isDefined(mainView) && mainView.isComponent) {
-                mainView.takePhoto().then(function (success) {
-                    Ext.toast(JSON.stringify(success));
-                }, function (error) {
-                    Ext.toast(error);
-                });
-            }
-        }
-    }, {
-        text: 'Gallery',
-        iconCls: 'x-fa fa-file-image-o'
-    }, {
-        xtype: 'tbfill'
-    }],
-
-    takePhoto: function (url) {
-        return new Ext.Promise(function (resolve, reject) {
-            Ext.Ajax.request({
-                url: '/photos/take',
-
-                success: function (response) {
-                    resolve(Ext.decode(response.responseText, true));
-                },
-
-                failure: function (response) {
-                    // Use the provided "reject" method to deliver error message.
-                    //
-                    reject(response.status);
+    items: [{
+        xtype: 'panel',
+        width: '80%',
+        layout: 'fit',
+        items: [{
+            xtype: 'container',
+            initComponent: function () {
+                var me = this;
+                this.callParent(arguments);
+                var mainView = me.up('mzkPiCameraMain');
+                if (Ext.isDefined(mainView)) {
+                    mainView.getPhotos().then(function (array) {
+                        var html = 'Keine Bilder vorhanden';
+                        if (array.length > 0) {
+                            var imgName = array[0].name;
+                            html = '<img src="/serve/' + imgName + '">';
+                        }
+                        me.setHtml(html);
+                    }, function (error) {
+                        Ext.toast(error);
+                    });
                 }
+            },
+        }],
+        bbar: [{
+            text: 'Take Picture',
+            iconCls: 'x-fa fa-photo',
+            handler: function (btn) {
+                var mainView = btn.up('mzkPiCameraMain');
+                if (Ext.isDefined(mainView) && mainView.isComponent) {
+                    mainView.takePhoto().then(function (success) {
+                        Ext.toast(JSON.stringify(success));
+                    }, function (error) {
+                        Ext.toast(error);
+                    });
+                }
+            }
+        }, {
+            text: 'Gallery',
+            iconCls: 'x-fa fa-file-image-o'
+        }, {
+            xtype: 'tbfill'
+        }],
+
+        takePhoto: function (url) {
+            return new Ext.Promise(function (resolve, reject) {
+                Ext.Ajax.request({
+                    url: '/photos/take',
+                    success: function (response) {
+                        resolve(Ext.decode(response.responseText, true));
+                    },
+
+                    failure: function (response) {
+                        reject(response.status);
+                    }
+                });
             });
-        });
-    }
+        },
+
+        getPhotos: function (url) {
+            return new Ext.Promise(function (resolve, reject) {
+                Ext.Ajax.request({
+                    url: '/photos',
+                    success: function (response) {
+                        resolve(Ext.decode(response.responseText, true));
+                    },
+                    failure: function (response) {
+                        reject(response.status);
+                    }
+                });
+            });
+        }
+    }]
 });
