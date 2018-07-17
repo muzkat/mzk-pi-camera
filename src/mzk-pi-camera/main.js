614fb43ef9a1bba29e4f52806a4aa8ef2ce15ceb
muzkat <erik@muzkat.com>
2018-07-17 20:14:31 +0200

    Improvements, JSON API, refactoring - seperating api and service functions

--- src/mzk-pi-camera/main.js
+++ src/mzk-pi-camera/main.js
@@ -13,26 +13,12 @@ Ext.define('muzkat.pi.camera.Main', {
         xtype: 'panel',
         width: '80%',
         layout: 'fit',
+
         items: [{
             xtype: 'container',
-            initComponent: function () {
-                var me = this;
-                this.callParent(arguments);
-                var mainView = me.up('mzkPiCameraMain');
-                if (Ext.isDefined(mainView)) {
-                    mainView.getPhotos().then(function (array) {
-                        var html = 'Keine Bilder vorhanden';
-                        if (array.length > 0) {
-                            var imgName = array[0].name;
-                            html = '<img src="/serve/' + imgName + '">';
-                        }
-                        me.setHtml(html);
-                    }, function (error) {
-                        Ext.toast(error);
-                    });
-                }
-            },
+            itemId: 'preview'
         }],
+
         bbar: [{
             text: 'Take Picture',
             iconCls: 'x-fa fa-photo',
@@ -51,35 +37,50 @@ Ext.define('muzkat.pi.camera.Main', {
             iconCls: 'x-fa fa-file-image-o'
         }, {
             xtype: 'tbfill'
-        }],
+        }]
+    }],
 
-        takePhoto: function (url) {
-            return new Ext.Promise(function (resolve, reject) {
-                Ext.Ajax.request({
-                    url: '/photos/take',
-                    success: function (response) {
-                        resolve(Ext.decode(response.responseText, true));
-                    },
+    initComponent: function () {
+        var me = this;
+        this.callParent(arguments);
+        me.getPhotos().then(function (array) {
+            var html = 'Keine Bilder vorhanden';
+            if (array.length > 0) {
+                var imgName = array[0].name;
+                html = '<img src="/serve/' + imgName + '">';
+            }
+            me.down('#preview').setHtml(html);
+        }, function (error) {
+            Ext.toast(error);
+        });
+    },
 
-                    failure: function (response) {
-                        reject(response.status);
-                    }
-                });
+    takePhoto: function (url) {
+        return new Ext.Promise(function (resolve, reject) {
+            Ext.Ajax.request({
+                url: '/photos/take',
+                success: function (response) {
+                    resolve(Ext.decode(response.responseText, true));
+                },
+
+                failure: function (response) {
+                    reject(response.status);
+                }
             });
-        },
+        });
+    },
 
-        getPhotos: function (url) {
-            return new Ext.Promise(function (resolve, reject) {
-                Ext.Ajax.request({
-                    url: '/photos',
-                    success: function (response) {
-                        resolve(Ext.decode(response.responseText, true));
-                    },
-                    failure: function (response) {
-                        reject(response.status);
-                    }
-                });
+    getPhotos: function (url) {
+        return new Ext.Promise(function (resolve, reject) {
+            Ext.Ajax.request({
+                url: '/photos',
+                success: function (response) {
+                    resolve(Ext.decode(response.responseText, true));
+                },
+                failure: function (response) {
+                    reject(response.status);
+                }
             });
-        }
-    }]
+        });
+    }
 });
